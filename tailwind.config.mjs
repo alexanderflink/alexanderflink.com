aaab986564ad822e76a5d56618ce6ac2c10508ff
Alexander Flink <hi@alexanderflink.com>
2024-07-25 22:09:53 +0200

    add dark mode support

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 import colors from "tailwindcss/colors";
-
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -9,9 +8,17 @@ export default {
         serif: ["DM Serif Display"],
         sans: ["DM Sans"],
       },
-      colors: {
-        black: colors.neutral[700],
-      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      inherit: "inherit",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.stone,
+      orange: colors.orange,
+      yellow: colors.yellow,
+      slate: colors.slate,
     },
   },
   plugins: [
